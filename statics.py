b39528590d398cfc2e01ecc8900bae5d7011143f
LeoWangCoder <zhwang110@gmail.com>
2021-09-16 15:33:38 -0400

    change 0 to 10

--- statics.py
+++ statics.py
@@ -6,7 +6,7 @@ classify_type = 'Classification'
 total = pd.DataFrame({'total': df.groupby(classify_type).size()})
 total = total.sort_values(['total'], ascending=False)
 total['2'] = total['total']
-total['total'] = total['total'].map(lambda x: x / 0)
+total['total'] = total['total'].map(lambda x: x / 10)
 print(total)
 
 
